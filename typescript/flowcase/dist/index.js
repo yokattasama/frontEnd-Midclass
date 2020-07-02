543082ac410a0188beddfedd87051a5ad432bd61
ml <2246888634@qq.com>
2020-07-03 00:06:34 +0800

    flow

---
+++ typescript/flowcase/dist/index.js
@@ -0,0 +1,4 @@
+// var a /* : number */ = 1;
+var a = 123;
+a = 456;
+console.log(a); // let b: Number = '123213';
---
+++ typescript/flowcase/src/index.js
@@ -0,0 +1,8 @@
+
+// @flow
+// var a /* : number */ = 1;
+var a: number = 123;
+a = 456;
+console.log(a);
+
+// let b: Number = '123213';
